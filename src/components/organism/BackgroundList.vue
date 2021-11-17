227a28b6e66b02ab943884626999fd6d29efdcad
s-mikio <sako.mikio@jp.fujitsu.com>
2021-11-17 12:38:18 +0900

    好きな画像を背景として設定できるようにした

--- src/components/organism/BackgroundList.vue
+++ src/components/organism/BackgroundList.vue
@@ -6,21 +6,28 @@
     </v-card-title>
     <v-row>
       <v-col v-for="item in backgrounds" :key="item.key" cols="4">
-        <v-card v-on:click="backgroundClicked(item)">
-          <v-img
-            :src="item.src.replace('../', '')"
-            class="white--text"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-icon
-              transition="scroll-x-transition"
-              color="#58acfc"
-              v-if="item.selected"
-              class="display-1"
-            >mdi-checkbox-marked-circle-outline</v-icon>
-          </v-img>
-        </v-card>
+        <div v-if="item.custom">
+          <!-- custom == true -->
+          <input type="file" accept=".jpg, .png" @change="handleSelectedImage($event, item)"/>
+        </div>
+        <!-- custom == false -->
+        <div v-else>
+          <v-card v-on:click="backgroundClicked(item)">
+            <v-img
+              :src="item.src.replace('../', '')"
+              class="white--text"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-icon
+                transition="scroll-x-transition"
+                color="#58acfc"
+                v-if="item.selected"
+                class="display-1"
+              >mdi-checkbox-marked-circle-outline</v-icon>
+            </v-img>
+          </v-card>
+        </div>
       </v-col>
     </v-row>
   </v-card>
@@ -38,6 +45,19 @@ export default {
       this.backgrounds.forEach(a => (a.selected = false));
       background.selected = true;
       this.$emit("selected", background);
+    },
+    handleSelectedImage(event, selectedItem){
+      // 登録された画像のURLを発行する
+      let file = event.target.files[0];
+      if(file == undefined || file == null){
+        return;
+      }
+      let url = URL.createObjectURL(file);
+
+      // 背景画像としてURLを設定する
+      selectedItem.src = url;
+
+      selectedItem.custom = false;
     }
   },
   data: () => ({
@@ -70,7 +90,8 @@ export default {
       {
         key: 6,
         src: "../bgs/6.jpg",
-        selected: false
+        selected: false,
+        custom: true
       }
     ]
   })
